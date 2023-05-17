6864d3d71d4c12c16c381d0eb9b4aa5950029bff
ayoidan <78206286+ayo-nci@users.noreply.github.com>
2023-05-17 15:05:32 +0100

    Added helloo world

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,5 +1,7 @@
 function Message() {
-    return <h1 className="">Hello World</h1>
+    const name = 'Mosh';
+    if (name) return <h1 className="">Hello {name}</h1>
+    return <h1 className="">Hello world</h1>
 }
 
 export default Message;
