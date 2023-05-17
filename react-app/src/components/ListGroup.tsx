ffe9d2a4b46f28366ccb3bee565ec787ac6f09a2
ayoidan <78206286+ayo-nci@users.noreply.github.com>
2023-05-17 16:52:24 +0100

    Added usestate

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,19 +1,35 @@
+import { useState } from 'react';
+
 function ListGroup() {
     // return <h1>List Group</h1>;
-    const items = [
+    let items = [
         'New York',
         'Lagos',
         'Dublin',
         'Peru',
         'Ghana'
     ];
+    // let selectedIndex = 0
 
+    //Event handler
+    const handleClick = (event: MouseEvent) => console.log(event)
 
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    
 
     return <>
-        <h1>List</h1>
+        <h1>Listt</h1>
+        {items.length === 0 && <p className="">No items found</p>}
         <ul className="list-group">
-            {items.map((item, index) => (<li key={index} className="list-group-item">{item}</li>))}
+            {items.map((item, index) =>
+            (<li
+                key={index}
+                onClick={() => setSelectedIndex(index)}
+                className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            >
+                {item}
+            </li>
+            ))}
         </ul>
     </>;
 
